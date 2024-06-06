1f34a5a266844ad713dd1a978b40e2ead30c5515
changukLee <ckddnr5527@gmail.com>
2024-06-06 21:43:18 +0900

    feat: update Text

--- app/src/main/java/com/example/jetpackcomposepractice/TextActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/TextActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposepractice.ui.theme.JetpackComposePracticeTheme
 
-class MainActivity : ComponentActivity() {
+class TextActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,11 +29,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyTextEx()
                 }
             }
         }
     }
+}
+
+@Composable
+private fun MyTextEx() {
+
+    // text
+    // fontSize
+    // color
+    // fontStyle
+    // fontWeight
+    // modifier(padding)
+    // background
+
+    Text(
+        text = "안녕하세요 텍스트 예제입니다.",
+        fontSize = 30.sp,
+        color = Color.Red,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(30.dp),
+        style = TextStyle(background = Color.Blue)
+    )
 }
 
 @Composable
@@ -41,6 +70,12 @@ private fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 private fun GreetingPreview() {
     JetpackComposePracticeTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MyTextEx()
+        }
     }
 }
