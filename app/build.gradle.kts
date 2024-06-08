ec4e221f66a6480c2cd910b1fbe4741f0212e3d3
changukLee <ckddnr5527@gmail.com>
2024-06-08 19:07:26 +0900

    feat: update Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,6 @@ dependencies {
 
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
---
+++ app/src/main/java/com/example/jetpackcomposepractice/RetrofitActivity.kt
@@ -0,0 +1,156 @@
+package com.example.jetpackcomposepractice
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomposepractice.ui.theme.JetpackComposePracticeTheme
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+// https://jsonplaceholder.typicode.com/posts/1
+
+class RetrofitActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            /*  예제 1
+            JetpackComposePracticeTheme {
+
+                val coroutineScope = rememberCoroutineScope()
+                val retrofitInstance = RetrofitInstance.getInstance().create(MyApi::class.java)
+
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+                                val response = retrofitInstance.getPost1()
+                                Log.d("testt", response.body().toString())
+                            }
+                        }
+                    ) {
+                        Text(text = "CALL API")
+                    }
+                }
+            }
+
+ */
+            // 예제 2
+            JetpackComposePracticeTheme {
+                InsertInputData()
+            }
+        }
+    }
+
+    @Composable
+    fun InsertInputData() {
+        // 입력하는 숫자
+        var inputNumber by remember {
+            mutableStateOf("")
+        }
+
+        // 받아온 데이터 모델
+        var post by remember {
+            mutableStateOf<Post?>(null)
+        }
+
+        // API 통신하는 부분
+        val coroutineScope = rememberCoroutineScope()
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(
+                value = inputNumber,
+                onValueChange = {
+                    inputNumber = it
+                } ,
+                label = { Text(text = "값을 입력해주세요(숫자)")}
+            )
+
+            Button(
+                onClick = {
+                    val number = inputNumber.toIntOrNull()
+                    if (number != null) {
+                        coroutineScope.launch {
+                            post = getPostData(number)
+                        }
+                    }
+                }
+            ) {
+                Text(text = "데이터 받아오기")
+            }
+            post?.let {
+                Text(text = "UserId : " + it.userId)
+                Text(text = "Id : " + it.id)
+                Text(text = "Title : " + it.title)
+                Text(text = "Body : " + it.body)
+
+            }
+        }
+    }
+
+    private suspend fun getPostData(number: Int):Post? {
+        val retrofitInstance = RetrofitInstance.getInstance().create(MyApi::class.java)
+        val response = retrofitInstance.getPostNumber(number)
+        return if (response.isSuccessful) response.body() else null
+    }
+}
+
+data class Post(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
+)
+
+object RetrofitInstance {
+    val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+    val client = Retrofit
+        .Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun getInstance() :Retrofit {
+        return client
+    }
+}
+
+interface MyApi {
+    @GET("posts/1")
+    suspend fun getPost1(): Response<Post>
+
+    @GET("posts/{number}")
+    suspend fun getPostNumber(
+        @Path("number") number: Int
+    ): Response<Post>
+}
