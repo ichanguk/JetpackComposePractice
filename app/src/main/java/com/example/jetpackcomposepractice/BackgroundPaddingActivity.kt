f6bdf3b3296bb25422e2b6deb0cddfe9a32be94e
changukLee <ckddnr5527@gmail.com>
2024-06-10 19:41:37 +0900

    feat: update background padding 순서 차이

---
+++ app/src/main/java/com/example/jetpackcomposepractice/BackgroundPaddingActivity.kt
@@ -0,0 +1,97 @@
+package com.example.jetpackcomposepractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposepractice.ui.theme.JetpackComposePracticeTheme
+
+class BackgroundPaddingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposePracticeTheme {
+                MyBoxTest()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyBoxTest() {
+
+    // fillMaxWidth
+    // height
+    // background
+    // clip
+    // padding
+
+    Column() {
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .background(Color.Green)
+                .clip(RoundedCornerShape(30.dp))
+                .padding(20.dp)
+        ) {
+            Text(text = "test")
+        }
+
+        Spacer(modifier = Modifier.padding(20.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .clip(RoundedCornerShape(30.dp))
+                .background(Color.Green)
+                .padding(20.dp)
+        ) {
+            Text(text = "test")
+        }
+
+        Spacer(modifier = Modifier.padding(20.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(20.dp)
+                .clip(RoundedCornerShape(30.dp))
+                .background(Color.Green)
+
+        ) {
+            Text(text = "test")
+        }
+
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview31() {
+    JetpackComposePracticeTheme {
+        MyBoxTest()
+    }
+}
